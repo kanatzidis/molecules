84ecdd6563f5dc43bac85e1593d81090f27ef620
Gregori Kanatzidis <kanatzidisg@gmail.com>
2015-03-12 12:59:45 -0500

    add example

---
+++ example/test.js
@@ -0,0 +1,25 @@
+var Unzip = require('../index').Unzip;
+var fs = require('fs');
+var mkdirp = require('mkdirp');
+var path = require('path');
+
+fs.createReadStream('./archive.zip').pipe(Unzip()).on('entry', function(entry, header) {
+  console.log(header.filename);
+  console.log(JSON.stringify(header));
+
+  if(!header.folder) {
+    //console.log(path.dirname(header.filename));
+    mkdirp(path.dirname(header.filename), function(err) {
+      if(err) throw err;
+      entry.pipe(fs.createWriteStream(header.filename)).on('finish', function() {
+
+        console.log('Finished writing ' + header.filename);
+
+      });
+    });
+  }
+
+}).on('finish', function() {
+  console.log('Extraction complete.');
+});
+
